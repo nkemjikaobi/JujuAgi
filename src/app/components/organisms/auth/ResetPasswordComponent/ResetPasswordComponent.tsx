61c72a9d2f1294b507e749d0ebd1de1e45988f31
Obi Nkemjika Derick <obinkemjikaderick@macbook-pro.home>
2023-07-26 18:00:59 -0400

    Reset Password

--- src/app/components/organisms/auth/ResetPasswordComponent/ResetPasswordComponent.tsx
+++ src/app/components/organisms/auth/ResetPasswordComponent/ResetPasswordComponent.tsx
@@ -2,10 +2,13 @@
 import CustomButton from "@/app/components/atoms/CustomButton/CustomButton";
 import FormikCustomInput from "@/app/components/atoms/FormikCustomInput/FormikCustomInput";
 import Icon from "@/app/components/atoms/Icons";
-import { ButtonProperties, errorMessages } from "@/app/libs/helpers";
+import { showToast } from "@/app/components/atoms/ShowToast/showToast";
+import { RESET_PASSWORD } from "@/app/graphql/auth/mutations";
+import { ButtonProperties, NotificationTypes, Status, errorMessages } from "@/app/libs/helpers";
+import { useMutation } from "@apollo/client";
 import { Form, Formik, FormikProps } from "formik";
-import { useRouter } from "next/navigation";
-import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
 import { AnimateContainer } from "react-animate-container";
 import * as yup from "yup";
 import yupPassword from "yup-password";
@@ -13,6 +16,9 @@ yupPassword(yup); // extend yup
 
 const ResetPasswordComponent = () => {
   const router = useRouter();
+  const [resetPasswordHandler, { data, loading, error }] = useMutation(RESET_PASSWORD);
+  const searchParams = useSearchParams();
+
   const initialState = {
     password: "",
     confirmPassword: "",
@@ -39,8 +45,34 @@ const ResetPasswordComponent = () => {
   });
 
   const resetPassword = async (values: Values) => {
-    router.push("/auth/password-reset-success");
+    const token = searchParams.get("token");
+
+    await resetPasswordHandler({
+      variables: {
+        email: "",
+        newPassword: values.password,
+        token,
+      },
+    });
   };
+
+  useEffect(() => {
+    if (data) {
+      const { status, message } = data.resetPassword;
+      if (status === Status.SUCCESS) {
+        showToast(message, NotificationTypes.SUCCESS);
+        router.push("/auth/password-reset-success");
+      }
+      if (status === Status.FAILED || status === Status.ERROR) {
+        showToast(message, NotificationTypes.ERROR);
+      }
+    }
+
+    if (error) {
+      showToast("An error occurred", NotificationTypes.ERROR);
+    }
+    // eslint-disable-next-line
+  }, [data, error]);
 
   return (
     <AnimateContainer.fadeIn>
@@ -81,6 +113,8 @@ const ResetPasswordComponent = () => {
               <CustomButton
                 customClass="w-full rounded-[0.75rem]"
                 handleClick={() => {}}
+                isDisabled={loading}
+                isSubmitting={loading}
                 size={ButtonProperties.SIZES.big}
                 title="Reset Password"
                 type="submit"
